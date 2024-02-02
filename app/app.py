fe596446e8ee7e6f0f7e0be9a9364dabb56a3148
33-66 <simon.nganga@student.moringaschool.com>
2024-02-02 21:49:32 +0300

    Models created and  debug=True created

--- app/app.py
+++ app/app.py
@@ -15,8 +15,8 @@ db.init_app(app)
 
 @app.route('/')
 def home():
-    return ''
+    return '<h1>Hello World</h1>'
 
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555,debug=True)
--- app/models.py
+++ app/models.py
@@ -3,8 +3,22 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Restaurant(db.Model):
-    __tablename__ = 'Restaurant'
+    __tablename__ = 'Restaurants'
+# add any models you may need. 
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+class Pizza(db.Model):
+    __tablename__ = "pizzas"
 
-# add any models you may need. 
+    id = db.Column(db.Integer,primary_key=True)
+    name =db.Column(db.Integer)
+    ingridients = db.Column(db.String)
+class RestaurantPizza(db.Model):
+    __tablename__ =  "restaurant_pizzas"
+
+    id = db.Column(db.Integer, primary_key=True)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('Restaurants.id'))
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
+    price  = db.Column(db.Integer)    
